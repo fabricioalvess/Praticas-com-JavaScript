373137dbb4b42a3be59668392b7033899d5cfd4f
Fabrício Cintra <fabricioalvescintra@gmail.com>
2023-02-07 21:40:13 -0300

    Atualizacoes
    localStorage, metodos de tempo com setTimeout

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var sobre = [
     {
     "titulo":"Missao",
-    "texto":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    "texto":""
     },
     {
     "titulo": "Visao",
-    "texto":"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    "texto":""
     },
     {
-    "titulo":"Valores",
-    "texto":"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    "titulo":"valor",
+    "texto": ""
     }
 ]
 
@@ -39,11 +39,11 @@ document.getElementById("seta_sobre_frente").onclick = function(){
 
 //
 var servicos ={
-    "<p>web designer</p>":"<p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution</p>",
+    "<p>web designer</p>":"<p></p>",
 
-    "<p>frontend</p>":"<p>tletters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a </p>",
+    "<p>frontend</p>":"<p></p>",
 
-    "<p>backend</p>":"<p>sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like) </p>",
+    "<p>backend</p>":"<p></p>",
 }
 
 var unselected_color = "#646872";
@@ -87,6 +87,44 @@ window.setInterval(function(){
     }
 
     document.getElementById("relogio").innerHTML = formatarHora(hora)+" : "+ formatarHora(minuto) + " : "+ formatarHora(segundos)
-},1000)
+},1000);
 
+document.getElementById("boasVindas").style.display="none";
+
+document.getElementById("botaoLogin").onclick = function(){
+    
+    //salvar nome em localstorage
+    var nome = document.getElementById("nomeUsuario").value;
+    window.localStorage.setItem("nome", nome);
+    //esconder sessao login
+    document.getElementById("sessaoLogin").style.display="none";
+    document.getElementById("boasVindas").style.display="initial";
+    document.getElementById("boasVindasNomeLogin").innerHTML= "Ola "+ localStorage.nome;
+    document.getElementById("naoEUsuario").innerHTML = "nao é "+ localStorage.nome +"?";
+}
+if(localStorage.nome){
+    var nome = document.getElementById("nomeUsuario").value;
+    window.localStorage.setItem("nome", nome);
+    //esconder sessao login
+    document.getElementById("sessaoLogin").style.display="none";
+    document.getElementById("boasVindasNomeLogin").innerHTML= "Ola "+ localStorage.nome;
+    document.getElementById("naoEUsuario").innerHTML = "nao é "+ localStorage.nome +"?";
+}
+document.getElementById("naoEUsuario").onclick = function(){
+    localStorage.removeItem("nome");
+    document.getElementById("boasVindas").style.display="none";
+}
+
+
+
+
+
+    document.getElementById("mostrar-loader").onclick = function(){
+
+        document.getElementById("spinner-loader").style.display="initial";
+        
+        window.setTimeout(function(){
+            document.getElementById("spinner-loader").style.display="none";
+        },2000);
+    }
 
